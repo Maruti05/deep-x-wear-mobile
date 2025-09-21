16d73ec1e7ae4e611aab48075104fe58bdbd163d
Maruti05 <mangranim@gmail.com>
2025-09-21 13:05:26 +0530

    first commit mesg

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,9 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
+import { config } from '@/gluestack-ui.config';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { GluestackUIProvider } from '@gluestack-ui/themed';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -13,6 +15,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
+     <GluestackUIProvider config={config}>
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
@@ -20,5 +23,6 @@ export default function RootLayout() {
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
